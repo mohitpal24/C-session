d14d2c46981d7dd53b828c50e6b4b82ae88eac41
Mohit Pal <116500860+mohitpal24@users.noreply.github.com>
2022-11-01 22:38:01 +0530

    Create sum_of_digit_of_five_num.c

---
+++ sum_of_digit_of_five_num.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+    int sum = 0;
+
+    while (n > 0)
+    {
+        int mod = n % 10;
+        sum = sum + mod;
+        n = n / 10;
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
